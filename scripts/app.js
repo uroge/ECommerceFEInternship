63980b46450f314c7f0babb51053c099521827e1
uroge <67282212+uroge@users.noreply.github.com>
2021-03-26 13:06:55 +0100

    Implemented addToCartHandler to the ADD TO CART button and displayed added products in the cart

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,47 @@ const wallet = document.getElementById('wallet');
 const cart = document.querySelector('.cart-icon');
 const cartDropdown = document.querySelector('.cart-dropdown');
 
+const itemCount = document.querySelector('.item-count');
+const productsInCart = document.querySelector('.cart-items');
+
+const cartItems = {
+    totalPrice: 0,
+    products: []
+};
+
 cart.addEventListener('click', () => {
     cartDropdown.classList.toggle('closed');
 });
 
 wallet.textContent = eCommerceData.wallet;
 
+const addProductToCart = (productToAdd) => {
+    const newProduct = document.createElement('div');
+    newProduct.classList.add('cart-item');
+    newProduct.innerHTML = `
+        <img src=${productToAdd.productImage} alt="item"/>
+        <div class="item-details">
+            <span class="name">${productToAdd.productTitle}</span>
+            <span class="price">${productToAdd.productPrice}</span>
+        </div>
+    `;
 
+    productsInCart.appendChild(newProduct);
+};
+
+const addToCartHandler = (productToAdd) => {
+    
+    if(!cartItems.products.includes(productToAdd)) {
+        cartItems.products.push(productToAdd);
+        cartItems.totalPrice += productToAdd.productPrice;
+        itemCount.textContent = cartItems.products.length;
+        addProductToCart(productToAdd);
+        console.log(cartItems);
+    } else {
+
+    }
+    
+};
 
 const renderProducts = (product) => {
     const productPreview = document.createElement('div');
@@ -27,6 +61,8 @@ const renderProducts = (product) => {
         </div>
         <button class="custom-btn" id="add-to-cart">Add to cart</button>
     `;
+
+    productPreview.querySelector('button').addEventListener('click', () => addToCartHandler(product));
 
     return productPreview;
 };
